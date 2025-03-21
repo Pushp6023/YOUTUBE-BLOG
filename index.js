a9c85eedb930ce5c3e95f67b2354d254961c6ab9
pushp6023 <pushp1609@gmail.com>
2025-03-21 23:25:01 +0530

    Blogging Application

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 8000
+const PORT = 8001;
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
-
+app.get('/', (req, res) => {
+    res.render("home");
+});
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
