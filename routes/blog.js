73a044d486a788e1189f89069257947f92097996
pushp6023 <pushp1609@gmail.com>
2025-04-01 00:34:40 +0530

    Blogging Application

--- routes/blog.js
+++ routes/blog.js
@@ -4,6 +4,7 @@ const multer = require("multer");
 const path = require("path");
 const User = require("../models/user");
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, path.resolve(`./public/uploads/`));
@@ -33,12 +34,14 @@ router.get("/add-new", async (req, res) => {
 });
 router.get("/:id", async (req, res) => {
     const blog = await Blog.findById(req.params.id).populate('createdBy');
+    const comments = await Comment.find({ blogId: req.params.id}).populate('createdBy');
     if(!req.user)
     {
         return res.render("blog", {
             user: req.user,
             compUser: null,
             blog,
+            comments,
         });
     }
     else
@@ -48,8 +51,17 @@ router.get("/:id", async (req, res) => {
             user: req.user,
             compUser: completeUser,
             blog,
+            comments,
         });
     }
+});
+router.post("/comment/:blogId", async (req, res) => {
+    const comment = await Comment.create({
+        content: req.body.content,
+        blogId: req.params.blogId,
+        createdBy: req.user._id,
+    });
+    return res.redirect(`/blog/${req.params.blogId}`);
 });
 router.post("/", upload.single("coverImage"), async (req, res) => {
     const {title, body} = req.body;
